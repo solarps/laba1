b0951ff9a9a4ecde9b1e2eb6193b9b9a4cdd3b93
solarps <55921834+solarps@users.noreply.github.com>
2020-03-06 19:07:48 +0200

    Update Program.cs

--- laba1/Program.cs
+++ laba1/Program.cs
@@ -3,14 +3,20 @@ using System;
 namespace Lab1
 {
     class Program
-    { 
+    {
+        static double task(double x, double y)
+        {
+            double razn = x - y;
+            return razn;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Enter first number");
             double x = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter second number");
             double y = double.Parse(Console.ReadLine());
-            Console.WriteLine("result=" + (x - y));
+            Console.WriteLine("result=" + task(x,y));
+            Console.ReadLine();
         }
     }
 }
